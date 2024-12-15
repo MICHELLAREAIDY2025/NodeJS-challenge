faf10d518a84d2439a3a3eaf6152b0441e766087
Michella <michella.reaidy@gmail.com>
2024-12-15 21:53:37 +0200

    split

--- removenewline.js
+++ removenewline.js
@@ -6,3 +6,17 @@ const cleanedText = textWithNewlines.replace(/\n/g, ' ');
 
 // Print the cleaned text
 console.log(cleanedText);
+
+
+// Sample text
+const text = "command firstWord secondWord thirdWord";
+
+// Split the string into words
+const words = text.split(' ');
+
+// Extract the first word as the command
+const command = words[0];
+
+// Print the command and the rest of the words
+console.log("Command:", command);
+console.log("Words:", words);
